11228e52d4ff2f23f7fb0d3b53b8ff632b16b7f0
xNazim <nazim.muktarbekov@iaau.edu.kg>
2022-05-22 15:10:07 +0600

    first commit

---
+++ src/main/java/com/example/backendwebfinal/BackendWebFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendwebfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWebFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendWebFinalApplication.class, args);
+    }
+
+}
