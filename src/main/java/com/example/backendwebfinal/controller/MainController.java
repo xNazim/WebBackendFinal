33a3975329b9b5bf86614366fb0ac72427aa0bed
zhakyp <zhoomartuuluzhakyp@gmail.com>
2022-05-26 14:23:14 +0600

    changed files

---
+++ src/main/java/com/example/backendwebfinal/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.example.backendwebfinal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("index")
+    public String index(){
+        return index();
+    }
+
+
+}
