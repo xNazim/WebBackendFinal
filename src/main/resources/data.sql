8c69fccb2e736eec80bfff2084713bb2362a02a1
xNazim <nazim.muktarbekov@iaau.edu.kg>
2022-06-05 22:32:15 +0600

    init data updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,9 @@ insert into laboratory (id, lab_name, address, phone) values (2, 'Lab2', '5960 S
 insert into laboratory (id, lab_name, address, phone) values (3, 'Lab3', '856 Corry Court', '0555454455');
 insert into laboratory (id, lab_name, address, phone) values (4, 'Lab4', '131 Sauthoff Way', '0999458595');
 insert into laboratory (id, lab_name, address, phone) values (5, 'Lab5', '27 Straubel Parkway', '0502456545');
+
+insert into doctors (id, first_name, last_name, specialty, address, mobile_number) values (1, 'Emmie', 'Baldick', 'Therapist', '79 Garrison Way', '0777777');
+insert into doctors (id, first_name, last_name, specialty, address, mobile_number) values (2, 'Corinne', 'Limpkin', 'Pediatrician', '593 Debs', '080788888');
+insert into doctors (id, first_name, last_name, specialty, address, mobile_number) values (3, 'Abe', 'Bourdel', 'Neurologist', '98 Center', '05505555');
+insert into doctors (id, first_name, last_name, specialty, address, mobile_number) values (4, 'Meir', 'Labrum', 'Dermotologist', '5 MapleWood', '07777777');
+insert into doctors (id, first_name, last_name, specialty, address, mobile_number) values (5, 'Elaina', 'Hardin', 'Psychiatrist', '53 Independence ', '07777777');
