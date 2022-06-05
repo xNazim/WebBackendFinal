54c4f41fa659084deca5e424a542d529d3fbb145
xNazim <nazim.muktarbekov@iaau.edu.kg>
2022-06-05 22:21:46 +0600

    initial data changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,8 @@ INSERT INTO users (first_name, last_name, username, password, enabled) VALUES ('
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
 
 
-insert into laboratory (id, labName, address, phone) values (1, 'Lab1', '83 Chinook Crossing', '077775558');
-insert into laboratory (id, labName, address, phone) values (2, 'Lab2', '5960 Schmedeman Lane', '0708454545');
-insert into laboratory (id, labName, address, phone) values (3, 'Lab3', '856 Corry Court', '0555454455');
-insert into laboratory (id, labName, address, phone) values (4, 'Lab4', '131 Sauthoff Way', '0999458595');
-insert into laboratory (id, labName, address, phone) values (5, 'Lab5', '27 Straubel Parkway', '0502456545');
+insert into laboratory (id, lab_name, address, phone) values (1, 'Lab1', '83 Chinook Crossing', '077775558');
+insert into laboratory (id, lab_name, address, phone) values (2, 'Lab2', '5960 Schmedeman Lane', '0708454545');
+insert into laboratory (id, lab_name, address, phone) values (3, 'Lab3', '856 Corry Court', '0555454455');
+insert into laboratory (id, lab_name, address, phone) values (4, 'Lab4', '131 Sauthoff Way', '0999458595');
+insert into laboratory (id, lab_name, address, phone) values (5, 'Lab5', '27 Straubel Parkway', '0502456545');
