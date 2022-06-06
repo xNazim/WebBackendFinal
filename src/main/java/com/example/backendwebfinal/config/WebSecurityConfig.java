15aaf83b32264f236144c17dae02b417505c9ded
xNazim <nazim.muktarbekov@iaau.edu.kg>
2022-06-06 13:44:37 +0600

    some changes

--- src/main/java/com/example/backendwebfinal/config/WebSecurityConfig.java
+++ src/main/java/com/example/backendwebfinal/config/WebSecurityConfig.java
@@ -41,8 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                csrf().disable()
+        http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/registration", "registration", "/donation", "/css/**", "/js/**", "/img/**").permitAll()
                 .antMatchers("/admin/**").hasAnyAuthority("ADMIN")
